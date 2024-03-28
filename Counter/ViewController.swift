4809ebe37404654f1ab77a0758d2682959a21e30
Dmitrii <landamanda223@yadex.ru>
2024-03-28 10:10:21 +0200

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        var count = 0
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "\(count)"
+        label.font = UIFont.systemFont(ofSize: 80)
+      
+        
+        
+                button.setTitle("Plus One", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
     }
 
+    @IBAction func buttonPlusOne(_ sender: UIButton) {
+        count += 1
+        label.text = "\(count)"
+    }
+    
 
-}
+    }
+    
+
 
